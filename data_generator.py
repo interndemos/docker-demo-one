31f01aa617642b19f115ea9601434b4874e16846
sd <denisov.univ@gmail.com>
2023-03-30 15:16:09 +0700

    Simplest version

--- data_generator.py
+++ data_generator.py
@@ -18,10 +18,10 @@ if __name__ == '__main__':
     with get_db_connection() as conn:
         with conn.cursor() as cur:
             cur.execute("DROP TABLE IF EXISTS products")
-            cur.execute("CREATE TABLE products (product_id SERIAL PRIMARY KEY , product_name varchar(255), product_category varchar(255))")
+            cur.execute("CREATE TABLE products (id SERIAL PRIMARY KEY , name varchar(255), category varchar(255))")
 
-            cur.execute("INSERT INTO products VALUES (%s, %s)", ('Lego Mindstorm', 'toys'))
-            cur.execute("INSERT INTO products VALUES (%s, %s)", ('Lego City', 'toys'))
-            cur.execute("INSERT INTO products VALUES (%s, %s)", ('Leather ball', 'sport'))
+            cur.execute("INSERT INTO products (name, category) VALUES (%s, %s)", ('Lego Mindstorm', 'toys'))
+            cur.execute("INSERT INTO products (name, category) VALUES (%s, %s)", ('Lego City', 'toys'))
+            cur.execute("INSERT INTO products (name, category) VALUES (%s, %s)", ('Leather ball', 'sport'))
 
             conn.commit()
